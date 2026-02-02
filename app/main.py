a5595c4bac63a0abdbffca8c9842201fb9cca35a
Rumanzi <tpatrickrumanzi@gmail.com>
2026-02-02 18:48:12 +0000

    add fall back for non existing locations or cuisines

--- app/main.py
+++ app/main.py
@@ -16,7 +16,8 @@ model=os.getenv("MODEL_NAME")
 
 llm=init_chat_model(
     api_key=api_key,
-    model=model
+    model=model,
+    max_tokens=200 #limit tokens to save money for now
 )
 
 template=PromptTemplate(
@@ -24,8 +25,10 @@ template=PromptTemplate(
     template="""
     you are a helpful assistant that  provide restaurant recommendation for the party based on user's {location},{budget_per_person},{cuisine},{number_of_people}
     {location} is the user's city of interest, {budget_per_person} is the maximum budget to be used by each person, {cuisine} is the cuisine type of interest
-    (mexican,indian,american etc) and {number_of_people} is the total number of people attending.
-    answer as clear as possible to help user decide the best restaurant.
+    (mexican,indian,american etc) and {number_of_people} is the total number of people attending. Avoid giving information about fictional places or cuisines.
+    if the location or cuisine is  fictional please answer: I don't know
+
+    answer as clear as possible with restaurants options to help the user choose the best one.
     """
 )
 
@@ -33,7 +36,7 @@ template=PromptTemplate(
 
 st.title("Ask me about restaurant recommendations...")
 location=st.text_input("what is your location of interest? ")
-budget_per_person=st.number_input("what is your budget_per_person?$$ ")
+budget_per_person=st.number_input("what is your budget_per_person?")
 cuisine=st.text_input("what is your cuisine of interest?")
 number_of_people=st.number_input("How many people coming?")
 
