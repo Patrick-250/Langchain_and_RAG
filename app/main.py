88923cb361b8e49f5ddf8b7e1a1d518f08de5dc2
Rumanzi <tpatrickrumanzi@gmail.com>
2026-02-02 18:36:30 +0000

    restaurant recommendations based on requirements

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate,PromptTemplate
+from langchain_core.globals import set_debug
 
+
+
+# set_debug(True)
 
 load_dotenv()
 api_key=os.getenv("OPENAI_API_KEY")
@@ -15,11 +20,32 @@ llm=init_chat_model(
 )
 
 template=PromptTemplate(
-    variables=["technical_term"],
-    template="provide a one line definition of {technical_term}"
+    variables=["location","budget_per_person","cuisine","number_of_people"],
+    template="""
+    you are a helpful assistant that  provide restaurant recommendation for the party based on user's {location},{budget_per_person},{cuisine},{number_of_people}
+    {location} is the user's city of interest, {budget_per_person} is the maximum budget to be used by each person, {cuisine} is the cuisine type of interest
+    (mexican,indian,american etc) and {number_of_people} is the total number of people attending.
+    answer as clear as possible to help user decide the best restaurant.
+    """
 )
-technical_term=input("what Technical term would you wanna learn about?  ")
-prompt=template.format(technical_term=technical_term)
-response=llm.invoke(prompt)
-print(response.content)
+
+
+
+st.title("Ask me about restaurant recommendations...")
+location=st.text_input("what is your location of interest? ")
+budget_per_person=st.number_input("what is your budget_per_person?$$ ")
+cuisine=st.text_input("what is your cuisine of interest?")
+number_of_people=st.number_input("How many people coming?")
+
+prompt=template.format(location=location,budget_per_person=budget_per_person,cuisine=cuisine,number_of_people=number_of_people)
+
+if location and budget_per_person and cuisine and number_of_people:
+    response=llm.invoke(prompt)
+    st.write(response.content)
+else:
+    st.write("please provide all the fields")
+
+
+
+
 
