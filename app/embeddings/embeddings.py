e871c97aa5263a741711894e8edaabf095a983dc
Patrick Rumanzi <tpatrickrumanzi@gmail.com>
2026-02-03 18:49:59 +0000

    add embeddings similarity score using numpy dot() method

--- app/embeddings/embeddings.py
+++ app/embeddings/embeddings.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
 
@@ -11,7 +12,15 @@ embedding_model=OpenAIEmbeddings(api_key=api_key)
 question1="the day is sunny with blue sky"
 question2="it is a blue sky sunny day"
 
-#gonna compare the similariuty between question1 and question2.... the embeddings looks kind more similar
+#gonna compare the similariuty between question1 and question2.... the embeddings should looks kind more similar
 
 embedding1=embedding_model.embed_query(question1)
-print(embedding1)
+# print(embedding1)
+
+embedding2=embedding_model.embed_query(question2)
+# print(embedding2)
+
+similarity_score=np.dot(embedding1,embedding2) #between 0-1
+
+print(similarity_score) #outputted 0.9653976068670587 meaning high similarity
+
