6975a0f2ad91c46623f407c3697d1fa11a271bab
Rumanzi <tpatrickrumanzi@gmail.com>
2026-03-10 13:17:41 -0500

    move add mcp server

--- app/MCP/mcp_server.py
+++ app/MCP/mcp_server.py
@@ -1,39 +1,16 @@
-import wikipedia
 
 from mcp.server.fastmcp import FastMCP
-
 mcp=FastMCP()
 
 @mcp.tool()
-def wikipedia_search(query:str)->str:
-    try:
-        return wikipedia.summary(query,sentences=2)
-    except Exception as e:
-        return "Error happend on my end"
-@mcp.tool()
-def get_me(name:str)->str:
-    if name=="Patrick Rumanzi":
-        return """
-         Software Engineer with experience delivering scalable, secure, and high performing systems. Interested
-in AI/ML and data engineering, passionate about figuring out how things work and enjoy using tech to
-solve real business problems. With experience from Tech companies including Scale AI, HCLTech and
-others, thrive on solving complex problems and making real differences. Also, passionate about
-continuous learning- especially in the exciting world of AI- and enjoy mentoring and helping others to
-foster growth and innovation. Enjoy contributing to different open-source Repositories that mostly
-resonate with me. My current role as Senior Technical lead at HCLTECH provided the opportunity to
-manage cross-functional teams to deliver large scale solutions. Open to volunteering for Gender equality,
-Animal welfare and environment sustainabilitySoftware Engineer with experience delivering scalable, secure, and high performing systems. Interested
-in AI/ML and data engineering, passionate about figuring out how things work and enjoy using tech to
-solve real business problems. With experience from Tech companies including Scale AI, HCLTech and
-others, thrive on solving complex problems and making real differences. Also, passionate about
-continuous learning- especially in the exciting world of AI- and enjoy mentoring and helping others to
-foster growth and innovation. Enjoy contributing to different open-source Repositories that mostly
-resonate with me. My current role as Senior Technical lead at HCLTECH provided the opportunity to
-manage cross-functional teams to deliver large scale solutions. Open to volunteering for Gender equality,
-Animal welfare and environment sustainability
-        """
-    else:
-        return None
+def make_reservation():
+    
+    """
+    use this tool when you need to make a reservation for a specific restaurant
+    """
+    return """ I booked a reservationa dn sent you a confirmation email"""
+
+
 if __name__ == "__main__":
     print("MCP SERVER started")
     mcp.run(transport="streamable-http")
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain.agents import create_agent
 from langchain.tools import tool
 import streamlit as st
 from langchain_tavily import TavilySearch
+import asyncio
 
 load_dotenv()
 
@@ -16,6 +18,15 @@ llm=init_chat_model(
     model="gpt-4o-mini"
 )
 
+client=MultiServerMCPClient({
+        "tools":{
+        "url":"http://localhost:8000/mcp",
+        "transport":"streamable_http"
+    }
+}
+
+
+)
 # tavily=TavilyClient() #used langchain tavily internal
 
 reAct_system_prompt="""
@@ -24,16 +35,11 @@ Your purpose is to help users discover exceptional dining experiences tailored p
 including location, cuisines and availability.
 
 """
-@tool
-def make_reservation():
-    """
-    this tool is used to book a reservation for a chosen restaurant.
-    
-    """
-    return """I have made you a reservationa and sent you a confirmation email.. enjoy your stay"""
+
+mcp_tools=asyncio.run(client.get_tools()) 
 
 
-tools=[TavilySearch(),make_reservation] 
+tools=[TavilySearch(),*mcp_tools]  
 agent=create_agent(
     model=llm,
     tools=tools,
@@ -46,9 +52,9 @@ st.title("Your AI agent")
 
 user_message=st.text_input("provide your criteria and i ll help you find a restaurant and make a reservation")
 if user_message:
-    result=agent.invoke({
+    result=asyncio.run(agent.ainvoke({
         "messages":[{"role":"user","content":user_message}]
-    })
+    })) #used asyncio to wrap and Use async agent execution
     final_message=result["messages"][-1]
 
     st.write(final_message.content)
